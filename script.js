31e6e9597a05efb098757755579045d095be1756
vanshu-himself <124697864+vanshu-himself@users.noreply.github.com>
2023-04-24 18:45:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function fibonacci(num) {
      
     // function returns the Fibonacci number
    
-    if (num <= 1){
-        return num;}
+    if (num <= 1)
+        return num;
     return fib(num-1) + fib(num-2);
 }
 
